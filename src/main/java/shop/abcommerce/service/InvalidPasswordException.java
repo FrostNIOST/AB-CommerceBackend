b55cf023ab66415f6232f5824ccc778edc38d7a9
Santiago Felipe Martinez Pineda <149542358+FrostNIOST@users.noreply.github.com>
2026-06-25 15:37:02 -0500

    depuracion de datos no relacionados

--- src/main/java/shop/abcommerce/service/InvalidPasswordException.java
+++ src/main/java/shop/abcommerce/service/InvalidPasswordException.java
@@ -8,6 +8,6 @@ public class InvalidPasswordException extends RuntimeException {
     private static final long serialVersionUID = 1L;
 
     public InvalidPasswordException() {
-        super("Incorrect password");
+        super("Contraseña incorrecta!");
     }
 }
